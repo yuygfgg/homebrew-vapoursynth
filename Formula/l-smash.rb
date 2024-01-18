4f8e4c24ae3d517120c67a6e8f5f19623b3a3dbe
Rainbaby <rainbaby@outlook.jp>
2024-01-18 00:02:01 +0000

    l-smash: add 2.18.0 bottle.

--- Formula/l-smash.rb
+++ Formula/l-smash.rb
@@ -6,6 +6,13 @@ class LSmash < Formula
   license "ISC"
   head "https://github.com/vimeo/l-smash.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/saindriches/homebrew-vapoursynth/releases/download/l-smash-2.18.0"
+    sha256 cellar: :any,                 arm64_ventura: "002dac9ec065cadfd7574227b13132d5a910f3c1c9bf263a2a0193b577877a61"
+    sha256 cellar: :any,                 ventura:       "c9549624924d1be8f5f3c488b0c26b886158eba0c47bbc477a767cb258af5441"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e7533e738dcc34ec0331ae96cd928762a24e0a9606c28dab62448cf07ae059a1"
+  end
+
   depends_on "saindriches/vapoursynth/obuparse"
 
   def install
