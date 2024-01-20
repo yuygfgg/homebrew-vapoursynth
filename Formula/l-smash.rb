e41d43ce8ff09d724c67168d450ad8caf98acbde
Rainbaby <rainbaby@outlook.jp>
2024-01-20 06:24:42 +0000

    l-smash: update 2.18.0_1 bottle.

--- Formula/l-smash.rb
+++ Formula/l-smash.rb
@@ -8,10 +8,10 @@ class LSmash < Formula
   head "https://github.com/vimeo/l-smash.git", branch: "master"
 
   bottle do
-    root_url "https://github.com/saindriches/homebrew-vapoursynth/releases/download/l-smash-2.18.0"
-    sha256 cellar: :any,                 arm64_ventura: "002dac9ec065cadfd7574227b13132d5a910f3c1c9bf263a2a0193b577877a61"
-    sha256 cellar: :any,                 ventura:       "c9549624924d1be8f5f3c488b0c26b886158eba0c47bbc477a767cb258af5441"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e7533e738dcc34ec0331ae96cd928762a24e0a9606c28dab62448cf07ae059a1"
+    root_url "https://github.com/saindriches/homebrew-vapoursynth/releases/download/l-smash-2.18.0_1"
+    sha256 cellar: :any,                 arm64_ventura: "4bad42799c0840af427717b6b95d1d6d0d64a32593eb131d6842d3ee38ca8d35"
+    sha256 cellar: :any,                 ventura:       "e425f51161dad8dca817597d6594446768ffdb203842fc4a1a897a2a8948b214"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a82061c6f8b17cb85a2b2fdd05a61b05a02f76098d78147ed27e025a9fdb9737"
   end
 
   depends_on "saindriches/vapoursynth/obuparse"
