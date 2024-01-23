ea87fe917d4cab94cd2ce0fc0df6215caae53a7a
Rainbaby <rainbaby@outlook.jp>
2024-01-23 21:56:35 +0000

    dovi: add 2.1.0 bottle.

--- Formula/dovi.rb
+++ Formula/dovi.rb
@@ -11,6 +11,13 @@ class Dovi < Formula
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    root_url "https://github.com/saindriches/homebrew-vapoursynth/releases/download/dovi-2.1.0"
+    sha256 cellar: :any,                 arm64_ventura: "3142ac9114cd3f3a1fe4f9c97704984b1a8cda7cadf38bf1449b5aef41708df4"
+    sha256 cellar: :any,                 ventura:       "1ec68c7db3d907c30adc7e20fd805bf26f85fef039820afe11fbd96c023e4c5a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b613b7ef5fb45d220431e0b194ad30e0a7b2787b457a0ac7532fdf8f06800a55"
+  end
+
   depends_on "rust" => :build
 
   def install
