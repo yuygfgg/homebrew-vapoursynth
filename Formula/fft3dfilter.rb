0e2fa40e3009d3a55e41115142cf4bcacd941a24
Rainbaby <rainbaby@outlook.jp>
2024-01-26 01:04:25 +0000

    fft3dfilter: add 2 bottle.

--- Formula/fft3dfilter.rb
+++ Formula/fft3dfilter.rb
@@ -6,6 +6,13 @@ class Fft3dfilter < Formula
   license "GPL-2.0-only"
   head "https://github.com/myrsloik/VapourSynth-FFT3DFilter.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/saindriches/homebrew-vapoursynth/releases/download/fft3dfilter-2"
+    sha256 cellar: :any, arm64_ventura: "1619258867870f43ff13d6d20c0a09c72abe392c3b144ac560b144fbe9f9a9a9"
+    sha256 cellar: :any, ventura:       "7549cba7b3ce0a07b30172e9cf9c96588957df520652d0a44e563732fd15c2cf"
+    sha256               x86_64_linux:  "194fa1ccbd9af638027f8208a2a91e4331de7b72f20423e0c83347eb4918a2b3"
+  end
+
   depends_on "cmake" => :build
   depends_on "meson" => :build
   depends_on "ninja" => :build
