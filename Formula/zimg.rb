052c85f2e65e2ced4f76d304d5047237f3839d37
yuygfgg <140488233+yuygfgg@users.noreply.github.com>
2025-02-28 09:11:30 +0800

    Create zimg.rb

---
+++ Formula/zimg.rb
@@ -0,0 +1,36 @@
+class Zimg < Formula
+  desc "Scaling, colorspace conversion, and dithering library"
+  homepage "https://github.com/sekrit-twc/zimg"
+  license "WTFPL"
+  head "https://github.com/sekrit-twc/zimg.git", branch: "master"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+
+  def install
+    ENV.append "CFLAGS", "-march=native"
+    ENV.append "CXXFLAGS", "-march=native"
+
+    system "./autogen.sh"
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~C
+      #include <assert.h>
+      #include <zimg.h>
+
+      int main()
+      {
+        zimg_image_format format;
+        zimg_image_format_default(&format, ZIMG_API_VERSION);
+        assert(ZIMG_MATRIX_UNSPECIFIED == format.matrix_coefficients);
+        return 0;
+      }
+    C
+    system ENV.cc, "test.c", "-L#{lib}", "-lzimg", "-o", "test"
+    system "./test"
+  end
+end
