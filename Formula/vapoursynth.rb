bf8e86545bf4c8fe285cdea000caba0ed974ae04
yuygfgg <140488233+yuygfgg@users.noreply.github.com>
2025-02-28 09:07:39 +0800

    Create vapoursynth.rb

---
+++ Formula/vapoursynth.rb
@@ -0,0 +1,67 @@
+class Vapoursynth < Formula
+  desc "Video processing framework with simplicity in mind"
+  homepage "https://www.vapoursynth.com"
+  license "LGPL-2.1-or-later"
+  head "https://github.com/yuygfgg/vapoursynth.git", branch: "arm_neon"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "cython" => :build
+  depends_on "libtool" => :build
+  depends_on "nasm" => :build
+  depends_on "pkgconf" => :build
+  depends_on "sse2neon" => :build
+  depends_on "python@3.13"
+  depends_on "zimg"
+
+  # std::to_chars requires at least MACOSX_DEPLOYMENT_TARGET=13.3
+  # so it is possible to avoid LLVM dependency on Ventura but the
+  # bottle would have issues if system was on macOS 13.2 or older.
+  on_ventura :or_older do
+    depends_on "llvm"
+    fails_with :clang
+  end
+
+  def install
+    if OS.mac? && MacOS.version <= :ventura
+      ENV.llvm_clang
+      ENV.prepend "LDFLAGS", "-L#{Formula["llvm"].opt_lib}/c++"
+    end
+
+    # 启用 march=native 进行编译优化
+    ENV.append "CFLAGS", "-march=native"
+    ENV.append "CXXFLAGS", "-march=native"
+
+    system "./autogen.sh"
+    inreplace "Makefile.in", "pkglibdir = $(libdir)", "pkglibdir = $(exec_prefix)"
+    system "./configure", "--disable-silent-rules",
+                          "--with-cython=#{Formula["cython"].bin}/cython",
+                          "--with-plugindir=#{HOMEBREW_PREFIX}/lib/vapoursynth",
+                          "--with-python_prefix=#{prefix}",
+                          "--with-python_exec_prefix=#{prefix}",
+                          *std_configure_args
+    system "make", "install"
+  end
+
+  def caveats
+    <<~EOS
+      This formula does not contain optional filters that require extra dependencies.
+      To use vapoursynth.core.sub, execute:
+        brew install vapoursynth-sub
+      To use vapoursynth.core.ocr, execute:
+        brew install vapoursynth-ocr
+      To use vapoursynth.core.imwri, execute:
+        brew install vapoursynth-imwri
+      To use vapoursynth.core.ffms2, execute the following:
+        brew install ffms2
+        ln -s "../libffms2.dylib" "#{HOMEBREW_PREFIX}/lib/vapoursynth/#{shared_library("libffms2")}"
+      For more information regarding plugins, please visit:
+        http://www.vapoursynth.com/doc/plugins.html
+    EOS
+  end
+
+  test do
+    system Formula["python@3.13"].opt_bin/"python3.13", "-c", "import vapoursynth"
+    system bin/"vspipe", "--version"
+  end
+end
