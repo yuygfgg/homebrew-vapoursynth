59d4089246430dd2b5450e7a8a57338139cfb254
yuygfgg <140488233+yuygfgg@users.noreply.github.com>
2025-02-28 09:21:36 +0800

    Update python-vapoursynth.rb

--- Formula/python-vapoursynth.rb
+++ Formula/python-vapoursynth.rb
@@ -1,15 +1,10 @@
 class PythonVapoursynth < Formula
   desc "Python wrapper of VapourSynth"
   homepage "https://www.vapoursynth.com"
-  url "https://github.com/vapoursynth/vapoursynth/archive/refs/tags/R65.tar.gz"
   sha256 "2bde5233b82d914b5e985119ed9cc344e3c27c3c068b5c4ab909176cd1751dce"
   license "LGPL-2.1-or-later"
-  head "https://github.com/vapoursynth/vapoursynth.git", branch: "master"
+  head "https://github.com/yuygfgg/vapoursynth.git", branch: "arm_neon"
 
-  livecheck do
-    url :stable
-    regex(/^R(\d+(?:\.\d+)*?)$/i)
-  end
 
   depends_on "python-setuptools" => :build
   depends_on "python@3.12" => :build
